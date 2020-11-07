ef9a9af9d44c27d72e017d2ee83eb83e31182bef
tzushimelon <4021761+tzushimelon@users.noreply.github.com>
2020-11-06 19:20:01 -0800

    test: Update example tests to be working

--- src/tests.rs
+++ src/tests.rs
@@ -1,9 +1,12 @@
-use super::{rocket, TemplateContext};
 
 use rocket::http::Method::*;
 use rocket::http::Status;
 use rocket::local::blocking::Client;
 use rocket_contrib::templates::Template;
+
+use crate::rocket;
+use crate::assets::ASSETS;
+use crate::model::context::TemplateContext;
 
 macro_rules! dispatch {
     ($method:expr, $path:expr, |$client:ident, $response:ident| $body:expr) => {{
@@ -15,16 +18,19 @@ macro_rules! dispatch {
 
 #[test]
 fn test_root() {
-    // Check that the redirect works.
-    for method in &[Get, Head] {
-        dispatch!(*method, "/", |client, response| {
-            assert_eq!(response.status(), Status::SeeOther);
-            assert!(response.body().is_none());
+    dispatch!(Get, "/", |client, response| {
+        let context = TemplateContext {
+            title: "Home",
+            name: None,
+            commands: None,
+            assets: &ASSETS,
+            parent: "layout",
+        };
 
-            let location: Vec<_> = response.headers().get("Location").collect();
-            assert_eq!(location, vec!["/hello/Unknown"]);
-        });
-    }
+        let expected = Template::show(client.rocket(), "index", &context).unwrap();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string(), Some(expected));
+    });
 
     // Check that other request methods are not accepted (and instead caught).
     for method in &[Post, Put, Delete, Options, Trace, Connect, Patch] {
@@ -40,28 +46,11 @@ fn test_root() {
 }
 
 #[test]
-fn test_name() {
-    // Check that the /hello/<name> route works.
-    dispatch!(Get, "/hello/Jack%20Daniels", |client, response| {
-        let context = TemplateContext {
-            title: "Hello",
-            name: Some("Jack Daniels".into()),
-            items: vec!["One", "Two", "Three"],
-            parent: "layout",
-        };
-
-        let expected = Template::show(client.rocket(), "index", &context).unwrap();
-        assert_eq!(response.status(), Status::Ok);
-        assert_eq!(response.into_string(), Some(expected));
-    });
-}
-
-#[test]
 fn test_404() {
     // Check that the error catcher works.
-    dispatch!(Get, "/hello/", |client, response| {
+    dispatch!(Get, "/helloworld/", |client, response| {
         let mut map = std::collections::HashMap::new();
-        map.insert("path", "/hello/");
+        map.insert("path", "/helloworld/");
 
         let expected = Template::show(client.rocket(), "error/404", &map).unwrap();
         assert_eq!(response.status(), Status::NotFound);
